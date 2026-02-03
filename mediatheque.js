69ddb8f52d1d49fa29406ad74dfc2df758b62907
champiloic <champiloic.210302@gmail.com>
2026-02-03 11:08:42 +0100

    affichage livre + description

--- mediatheque.js
+++ mediatheque.js
@@ -17,10 +17,13 @@ document.getElementById('query').addEventListener('input', async function () {
         const books = data.docs.slice(0, 5).map(book => {
             const title = book.title;
             const coverId = book.cover_i;
+            const key = book.key;
             const coverImg = coverId ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg` : '';
             return `<div>
                         <p>${title}</p>
-                        ${coverImg ? `<img src="${coverImg}" alt="${title}" />` : ''}
+                        ${coverImg ? `<a href="./detail.html?key=${key}" target="_blank">
+                                        <img src="${coverImg}" alt="${title}" />
+                                      </a>` : ''}
                     </div>`;
         }).join('');
         resultElement.innerHTML = books || '<p>Aucun résultat trouvé.</p>';
